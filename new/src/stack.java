dd39b4f1ae6a7e790347f91d9f9b32a789ac9f2b
justi <justi@192.168.254.3>
2018-07-05 21:08:53 -0400

    cool

--- new/src/stack.java
+++ new/src/stack.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class stack{
@@ -6,11 +8,14 @@ public class stack{
 	{
 		
 		Stack<String> mystack = new Stack<>();
+		Scanner scan = new Scanner(System.in);
+		String input;
 		
 		
 		mystack.push("Nathaniel");
 		mystack.push("JET");
 		mystack.push("data");
+		int q = 0;
 		
 		for(String i : mystack)
 		{
@@ -20,6 +25,55 @@ public class stack{
 
 		mystack.pop();
 		System.out.println("Whats on top of the stack?: " + mystack.peek()+  "\n ...cool!");
+		
+		for(int i = 0, ii = 5; i < ii; i++) 
+		{
+			System.out.println(i + " and " + ii);
+		}
+		
+		
+		for(;  q <= 10; q++)
+		{	
+		
+			System.out.println( " q " + q);
+		}
+		
+		for(;;)
+		{
+		System.out.println( "Computer: Knock knock?" );
+		input = scan.nextLine();
+		
+		if(input.equalsIgnoreCase("whos there") || input.equalsIgnoreCase("who's there") || input.equalsIgnoreCase("who is there") ||
+				input.equalsIgnoreCase("whos there?") || input.equalsIgnoreCase("who's there?") || input.equalsIgnoreCase("who is there?")) 
+		{
+			System.out.println( "Computer: Smartest" );
+			for(;;)
+			{
+			input = scan.nextLine();
+			
+			if(input.equalsIgnoreCase("smartest who")) 
+			{
+				System.out.println( "Computer: smartest AI in the world! " );
+				
+				  try {
+				        while (true) {
+				        	System.out.println( "Computer: hahahahhahahah " );
+				            Thread.sleep(5 * 500);
+				        }
+				    } catch (InterruptedException e) {
+				        e.printStackTrace();
+				    }
+				
+				  
+				  break;
+				
+			}else {System.out.println( "... it's Smartest" );}
+			
+			}
+			
+		}
+		
+		}
 		
 	}
 	
